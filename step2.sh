81790cc860e3df4066ff5ac7059abfb5afdaca0b
Bradley Taunt <brad@bt.ht>
2022-12-02 19:04:55 -0500

    Go basic to core basics, running into too many overlooked issues

--- step2.sh
+++ step2.sh
@@ -1,14 +1,8 @@
 #!/bin/ash
 
-rc-service dbus start
 rc-update add dbus
-
-rc-update add seatd
-rc-service seatd start
-
-rc-service lightdm start
 rc-update add lightdm
-
-setup-devd udev
+rc-service dbus start
+rc-service lightdm start
 
 reboot
